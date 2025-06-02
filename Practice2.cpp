e449e1cb77863775cf3a348b3795b244e0b02ee4
Moinkhan21 <moinmykhan07@gmail.com>
2025-06-02 09:41:51 +0530

    new code added

--- Practice2.cpp
+++ Practice2.cpp
@@ -98,4 +98,22 @@ int main() {
         k++;  // Increment total columns for the next row
         cout << endl;  // Move to next row
     }
+
+    // Separator lines for clarity between patterns
+    cout << "----------------------------------------" << endl;
+    cout << "----------------------------------------" << endl;
+    cout << "----------------------------------------" << endl;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < i+1; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (int i= n - 1; i > 0; i--) {
+        for (int j = 0; j < i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
 }
