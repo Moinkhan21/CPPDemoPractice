51a5ba948628f0cfb41c92bb13342c576c03da04
Moinkhan21 <moinmykhan07@gmail.com>
2025-06-02 11:35:52 +0530

    code done

--- Practice3.cpp
+++ Practice3.cpp
@@ -27,30 +27,5 @@ int main(){
         }
         cout << endl;  // Move to the next row
     }
-
-    // Separator lines for clarity between patterns
-    cout << "----------------------------------------" << endl;
-    cout << "----------------------------------------" << endl;
-    cout << "----------------------------------------" << endl;
-
-    int c = 1;
-
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<=i; j++) {
-            cout << c;  // Print the current number
-            c++;
-        }
-        cout << endl;  // Move to the next row
-    }
-
-    int start = c-n;
-    for(int i=0; i<n; i++) {
-        int k = start;
-        for(int j=0; j<n-i-1; j++) {
-            cout << k;  // Print the current number
-            k++;
-        }
-        start = start - (n-i-1);  // Update start for the next row
-        cout << endl;  // Move to the next row
-    }
 }
+
