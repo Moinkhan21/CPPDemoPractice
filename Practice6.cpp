21b7439e53316396899ca154d8802eb99c390b97
Moinkhan21 <moinmykhan07@gmail.com>
2025-06-03 10:55:32 +0530

    new code added

--- Practice6.cpp
+++ Practice6.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+bool checkEven(int n) {
+    if (n % 2 == 0) {  // Check if the number is even
+        return true;   // Return true if even
+    } else {
+        return false;  // Return false if odd
+    }
+}
+
 float circle_area(float radius){
         float area = 3.14 * radius * radius;  // Calculate the area of the circle
         return area;  // Return the calculated area
@@ -11,4 +19,14 @@ int main() {
     cin >> radius;  // Read the radius of the circle
     float area = circle_area(radius);  // Calculate the area of the circle
     cout << "Area of the circle is: " << area << endl;  // Output the area
+
+    int n;
+    cin >> n;
+    bool isEven = checkEven(n);  // Check if the number is even
+
+    if (isEven) {
+        cout << n << " is even number." << endl;  // Output if the number is even
+    } else {
+        cout << n << " is odd number." << endl;  // Output if the number is odd
+    }
 }
