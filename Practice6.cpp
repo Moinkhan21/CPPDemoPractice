62668a11a65a5cbd9a6dd63e18e34bfbb76ae9d3
Moinkhan21 <moinmykhan07@gmail.com>
2025-06-03 13:01:29 +0530

    code of factorial added

--- Practice6.cpp
+++ Practice6.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
 
+long long int factorial(long long int n) {
+
+    long long int fact = 1;  // Initialize factorial to 1
+
+    if (n == 0 || n == 1) {  // Base case: factorial of 0 or 1 is 1
+        return 1;            // Return 1 for base case
+    } else {
+        for(int i = 1; i <= n; i++) { // Loop from 0 to n-1
+            fact = fact * i;  // Multiply fact by the current number
+        }
+        return fact;
+    }
+}
+
 bool checkEven(int n) {
     if (n % 2 == 0) {  // Check if the number is even
         return true;   // Return true if even
@@ -47,4 +61,8 @@ int main() {
     } else {
         cout << num << " is even number." << endl;  // Output if the number is odd
     }
+
+    long long int ans = factorial(n);  // Calculate the factorial of n
+    cout << "Factorial of " << n << " is: " << ans << endl;  // Output the factorial
+    return 0;  // Return 0 to indicate successful execution
 }
