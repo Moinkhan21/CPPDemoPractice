e6581e4783a8545ac155e00c75353d1817a17b88
Moinkhan21 <moinmykhan07@gmail.com>
2025-06-10 12:57:32 +0530

    new code added

--- Practice10.cpp
+++ Practice10.cpp
@@ -10,6 +10,7 @@ int main() {
     cout << arr << endl; // This will print the address of the array
     cout << &arr << endl; // This will also print the address of the array
 
+    // Array initialization
     int arr1[] = {1, 2, 3, 4, 5};
     cout << "Array 1 created successfully." << endl;
 
@@ -17,7 +18,15 @@ int main() {
     cout << "Array 2 created successfully." << endl;
 
     int arr3[10] = {1, 2, 3, 4, 5}; // All elements initialized to 0
-    cout << "Array 3 created successfully." << endl;            
+    cout << "Array 3 created successfully." << endl; 
+
+    cout << arr1[0] << endl; // Accessing the first element of arr1
+    cout << arr2[4] << endl; // Accessing the first element of arr2
+
+    //Printing the array's vlaues
+    for(int i = 0; i < 5; i++) {
+        cout << arr1[i] << " ";
+    }
 
     return 0;
 }
