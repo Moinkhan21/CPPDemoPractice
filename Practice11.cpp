fd42594bbb911959754bdc2b869244c7a1033600
Moinkhan21 <moinmykhan07@gmail.com>
2025-06-12 10:08:49 +0530

    new code added

--- Practice11.cpp
+++ Practice11.cpp
@@ -14,6 +14,15 @@ void inc(int arr[], int size) {
     printArray(arr, size); // Print the array after incrementing
 }
 
+bool find(int arr1[], int size1, int key) {
+    for(int i = 0; i < size1; i++) {
+        if(arr1[i] == key) {
+            return true; // Key found
+        }
+    }
+    return false; // Key not found
+}
+
 int main() {
 
     int arr[] = {5, 6};
@@ -21,6 +30,19 @@ int main() {
     inc(arr, size);
 
     printArray(arr, size);
+
+    int arr1[5] = {1, 2, 3, 4, 5};
+    int size1 = 5;
+
+    cout << "Enter the key to find" << endl;
+    int key;
+    cin >> key;
+
+    if(find(arr1, size1, key)){
+        cout << "Key found in the array." << endl;
+    } else {
+        cout << "Key not found in the array." << endl;
+    }
 
     return 0;
 }
