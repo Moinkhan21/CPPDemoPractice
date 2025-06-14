2aa792042e2e61d33a21154cb8361e6482490bc7
Moinkhan21 <moinmykhan07@gmail.com>
2025-06-14 10:13:59 +0530

    Array's code added

--- Practice13.cpp
+++ Practice13.cpp
@@ -25,4 +25,26 @@ int main() {
         start++;
         end--;
     }
+
+    cout << endl;
+
+    int arr1[6] = {1, 2, 3, 4, 5, 6};
+    int size1 = 6;
+
+    int start1 = 0;
+    int end1 = size1 - 1;
+
+    while(start1 <= end1){
+
+        swap(arr1[start1], arr1[end1]);
+        start1++;
+        end1--;
+
+    }
+
+    for(int i = 0; i < size1; i++){
+        cout << arr1[i] << " ";
+    }
+
+    cout << endl;
 }
