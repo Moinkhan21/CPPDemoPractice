5411eab5db9cd4644b870b9321d23d654bae0ecf
Moinkhan21 <moinmykhan07@gmail.com>
2025-06-14 10:31:44 +0530

    new code added

--- Practice13.cpp
+++ Practice13.cpp
@@ -48,29 +48,51 @@ int main() {
 
     cout << endl;
 
-    int arr[8] = {10, 20, 30, 40, 50, 60, 70, 80};
-    int size = 8;
+    int arr2[7] = {10, 20, 30, 40, 50, 60, 70};
+    int size2 = 7;
 
-    int start = 0;
-    int end = size - 1;
+    int start2 = 0;
+    int end2 = size2 - 1;
 
-    while(start <= end){
+    while(start2 <= end2){
 
-        if(start > end){
+        if(start2 > end2){
             break;
         }
-        if(start == end){
-            cout << arr[start] << " ";
+        if(start2 == end2){
+            cout << arr2[start2] << " ";
         }
         else{
-            cout << arr[start] << " ";
-            cout << arr[end] << " ";
+            cout << arr2[start2] << " ";
+            cout << arr2[end2] << " ";
         }
 
-        start++;
-        end--;
+        start2++;
+        end2--;
     }
 
     cout << endl;
-    
+
+    int arr3[5] = {1, 2, 3, 4, 5};
+    int size3 = 5;
+
+    int start3 = 0;
+    int end3 = size3 - 1;
+
+    while(start3 <= end3){
+
+        swap(arr3[start3], arr3[end3]);
+        start3++;
+        end3--;
+
+    }
+
+    for(int i = 0; i < size3; i++){
+        cout << arr3[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+
 }
