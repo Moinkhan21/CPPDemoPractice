8864fa21ccd11843764b3b9cf83c093563bd6694
Moinkhan21 <moinmykhan07@gmail.com>
2025-06-15 10:56:16 +0530

    new code added

--- Practice14.cpp
+++ Practice14.cpp
@@ -12,5 +12,17 @@ int main() {
     cout << arr.size() << endl; // Output the size of the vector using the size() method
     cout << arr.capacity() << endl; // Output the capacity of the vector
 
+    // Add elements to the vector
+    arr.push_back(1);
+    arr.push_back(2);
+    arr.push_back(3);
+
+    //Printing array elements
+    for(int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " "; // Output each element in the vector
+    }
+    cout << endl; // New line after printing all elements
+
+
     return 0; // Return 0 to indicate successful execution
 }
