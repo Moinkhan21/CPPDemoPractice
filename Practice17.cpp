f01acff9b5ec6a5ee5626f7ee208199e3d340794
Moinkhan21 <moinmykhan07@gmail.com>
2025-06-18 12:10:07 +0530

    new code added

--- Practice17.cpp
+++ Practice17.cpp
@@ -4,5 +4,14 @@ using namespace std;
 
 int main() {
 
-    cout << "Hello, World!" << endl;
+    // Declare 2D Array
+    int arr[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    // Print the 2D Array
+    cout << "2D Array:" << endl;
+    cout << arr[1][2] << endl; // Accessing an element in the 2D array`
 }
