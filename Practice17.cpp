6505ef3cd8eb21d6010702672f97ff656e5c22df
Moinkhan21 <moinmykhan07@gmail.com>
2025-06-18 12:33:52 +0530

    printing array row & column wise code

--- Practice17.cpp
+++ Practice17.cpp
@@ -16,6 +16,7 @@ int main() {
     cout << arr[1][2] << endl; // Accessing an element in the 2D array`
 
     //row-wise print
+    cout << "Row-wise print:" << endl;
     // Outer loop 
     for(int i = 0; i < 3; i++) {
         //for every row, we need to print value in each column
@@ -24,4 +25,17 @@ int main() {
         }
         cout << endl; // New line after each row
     }
+
+    // Column-wise print
+    cout << "Column-wise print:" << endl;   
+
+    // Outer loop 
+    for(int i = 0; i < 3; i++) {
+        //for every row, we need to print value in each column
+        for(int j = 0; j < 3; j++) {
+            cout << arr[j][i] << " "; // Print each element in the row
+        }
+        cout << endl; // New line after each row
+    }
+
 }
