42fda834c101d0933d5d4956544804b4fdba2565
Moinkhan21 <moinmykhan07@gmail.com>
2025-07-12 16:27:04 +0530

    update code for positive and negative value

--- Practice43.cpp
+++ Practice43.cpp
@@ -4,18 +4,18 @@ using namespace std;
 
 int solve(int dividend, int divisor) {
     int s = 0;
-    int e = dividend;
+    int e = abs(dividend);
     int ans = 1;
 
     int mid = s + (e - s) / 2;
 
     while(s <= e) {
         //Perfect solution
-        if(mid*divisor == dividend) {
+        if(abs(mid*divisor) == abs(dividend)) {
             return mid; // Return the quotient
         }
         //Not perfect solution, but close
-        if(mid*divisor > dividend) {
+        if(abs(mid*divisor) > abs(dividend)) {
             //Left
             e = mid - 1;
         }
@@ -26,12 +26,16 @@ int solve(int dividend, int divisor) {
         }
         mid = s + (e - s) / 2; // Recalculate mid
     }
-    return ans; // Return the closest quotient found
+    if((divisor < 0 && dividend < 0) || (divisor > 0 && dividend > 0)) {
+        return ans; // Return the quotient
+    }else {
+        return -ans; // Return the negative quotient
+    }
 }
 
 int main() {
     int dividend = 22;
-    int divisor = 7;
+    int divisor = -7;
 
     int ans = solve(dividend, divisor);
     cout << "Ans is " << ans << endl;
