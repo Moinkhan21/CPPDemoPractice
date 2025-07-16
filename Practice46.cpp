3bdca42ecd5e9ac2e84ad09db1e33bd8a485b612
Moinkhan21 <moinmykhan07@gmail.com>
2025-07-16 11:51:48 +0530

    code improved with comments

--- Practice46.cpp
+++ Practice46.cpp
@@ -7,8 +7,10 @@ int main() {
     int n = arr.size();
 
     //Selection sort algorithm
+    //Outer loop -> (n - 1) rounds
     for(int i = 0; i < n - 1; i++) {
         int minIndex = i; // Assume the first element is the minimum
+        //Inner loop -> Find the minimum element in the unsorted part OR index of the minimum element in range [i, n]
         for (int j = i + 1; j < n; j++) {
             if (arr[j] < arr[minIndex]) {
                 minIndex = j; // Update minIndex if a smaller element is found
