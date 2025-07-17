960923e32ad4c433a1e3f4011b3ad4766eeeaa0d
Moinkhan21 <moinmykhan07@gmail.com>
2025-07-17 11:00:54 +0530

    code is optimized

--- Practice47.cpp
+++ Practice47.cpp
@@ -10,10 +10,17 @@ int main() {
 
     // Bubble sort algorithm
     for(int round = 1; round < n; round++) {
+
+            bool swapped = false; // Flag to check if any swapping occurred in this round
+        // Traverse through the array up to the last unsorted element
         for(int i = 0; i < n - round; i++) {
             if(arr[i] > arr[i + 1]) {
+                swapped = true; // Set the flag if a swap occurs
                 swap(arr[i], arr[i + 1]); // Swap if the current element is greater than the next
             }
+        }
+        if(!swapped) {
+            break; // If no swaps occurred, the array is sorted
         }
     }
 
