3099209d7e5e23d62f1fd9d404d41dc67773fbbe
Moinkhan21 <moinmykhan07@gmail.com>
2025-07-22 10:09:25 +0530

    2nd method for finding closest elements

--- Practice51.cpp
+++ Practice51.cpp
@@ -1,28 +1,64 @@
-class Solution {
-    public:
-     
-    vector<int> twoPtrMethod(vector<int>& arr, int k, int x){
-        int l = 0, h = arr.size() - 1;
+#include <iostream>
+#include <vector>
+using namespace std;
 
-        while(h - l >= k){
-            if(x - arr[l] > arr[h] - x){
-                l++;
-            }
-            else{
-                h--;
-            }
-        }
-        vector<int>ans;
-        for(int i = l; i <= h; i++){
-            ans.push_back(arr[i]);
-        }
-        return ans;
+/*
+---------------------------------------------------------
+Function: twoPointerMethod
+Purpose : Finds k closest elements to target x using the
+          two-pointer shrinking window approach.
+Params  :
+    - arr : sorted input array
+    - k   : number of closest elements to find
+    - x   : target value
+Returns :
+    A vector containing k closest elements to x
+---------------------------------------------------------
+*/
+vector<int> twoPointerMethod(vector<int>& arr, int k, int x) {
+    int left = 0;
+    int right = arr.size() - 1;
 
-        //Or
-        //return vector<int>(arr.begin() + l, arr.begin() + h + 1);
+    // Shrink the window size until it becomes exactly size k
+    while (right - left >= k) {
+        if (abs(x - arr[left]) > abs(arr[right] - x)) {
+            left++;  // Left element is farther, discard it
+        } else {
+            right--; // Right element is farther, discard it
+        }
     }
 
-    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        return twoPtrMethod(arr, k, x);
+    // Copy the resulting subarray of size k
+    return vector<int>(arr.begin() + left, arr.begin() + right + 1);
+}
+
+/*
+---------------------------------------------------------
+Function: findClosestElements
+Purpose : Wrapper function to call the actual method
+---------------------------------------------------------
+*/
+vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+    return twoPointerMethod(arr, k, x);
+}
+
+/*
+---------------------------------------------------------
+Main Function: Demonstrates working of the function
+---------------------------------------------------------
+*/
+int main() {
+    vector<int> arr = {1, 2, 3, 4, 5};
+    int k = 4;
+    int x = 3;
+
+    vector<int> result = findClosestElements(arr, k, x);
+
+    cout << "The " << k << " closest elements to " << x << " are:\n";
+    for (int num : result) {
+        cout << num << " ";
     }
+    cout << endl;
+
+    return 0;
 }
