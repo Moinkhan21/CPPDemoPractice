80045ed7a1a4000d6470d892f58bba256e2a2a31
Moinkhan21 <moinmykhan07@gmail.com>
2025-07-31 11:25:25 +0530

    question: removing substring part from main string

--- Practice63.cpp
+++ Practice63.cpp
@@ -1,32 +1,52 @@
-#include <iostream>
-#include <string>
+#include <iostream>     // For input and output streams
+#include <string>       // For using the string class and related functions
 using namespace std;
 
+/*
+ * Function: removeOccurrences
+ * ----------------------------
+ * Removes all non-overlapping occurrences of the substring `part` from the input string `s`.
+ * 
+ * Parameters:
+ *   - s    : the original string
+ *   - part : the substring that needs to be removed from `s`
+ * 
+ * Returns:
+ *   - A new string after removing all occurrences of `part` from `s`.
+ */
 string removeOccurrences(string s, string part){
+    // Find the first occurrence of `part` in `s`
     int pos = s.find(part);
+
+    // Continue removing `part` as long as it's found in `s`
     while(pos != string::npos){
+        // Erase the `part` from position `pos` with length equal to `part.length()`
         s.erase(pos, part.length());
+
+        // Search for the next occurrence of `part` in the modified string
         pos = s.find(part);
     }
+
+    // Return the final modified string
     return s;
 }
 
 int main() {
-    // Input a string
+    // Declare a string variable to hold the input string
     string str;
     cout << "Enter a string: ";
-    cin >> str;
+    cin >> str; // Take input for the string from the user
 
-    // Input the part to remove
+    // Declare a string variable to hold the part to remove
     string part;
     cout << "Enter the part to remove: ";
-    cin >> part;
+    cin >> part; // Take input for the substring to be removed
 
-    // Remove occurrences of part from str
+    // Call the function to remove all occurrences of `part` from `str`
     string result = removeOccurrences(str, part);
 
-    // Print the result
+    // Output the final string after removing the substring
     cout << "String after removing occurrences of '" << part << "': " << result << endl;
 
-    return 0;
+    return 0; // Return 0 to indicate successful program execution
 }
