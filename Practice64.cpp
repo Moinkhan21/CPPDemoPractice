37ab3fd0d8b03acc9f1872632e978995eb974caa
Moinkhan21 <moinmykhan07@gmail.com>
2025-08-02 11:31:14 +0530

    checking palindrome after removing at most one element

--- Practice64.cpp
+++ Practice64.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+bool checkPalindrome(string s, int i, int j) {
+    while(i <= j) {
+        if(s[i] != s[j]) {
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+}
+
+bool validPalindrome(string s){
+    int i = 0;
+    int j = s.size() - 1;
+
+    while(i <= j) {
+        if(s[i] != s[j]) {
+            //Either remove i or j character
+            return checkPalindrome(s, i + 1, j) || checkPalindrome(s, i, j - 1);
+        }
+        else {
+            //s[i] == s[j]
+            i++;
+            j--;
+        }
+    }
+    return true; // The string is a valid palindrome
+}
+
 int main () {
-    cout << "Hello, World!" << endl;
+    string s;
+    cout << "Enter a string: ";
+    cin >> s;
+
+    if(validPalindrome(s)) {
+        cout << "The string can be a valid palindrome after removing at most one character." << endl;
+    } else {
+        cout << "The string cannot be a valid palindrome even after removing one character." << endl;
+    }
+
+    return 0; // Return 0 to indicate successful program execution
 }
