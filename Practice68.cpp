7601fc5823d7514ca1301b9a0b1b445d86844764
Moinkhan21 <moinmykhan07@gmail.com>
2025-08-04 10:06:23 +0530

    map datastructure

---
+++ Practice68.cpp
@@ -0,0 +1,18 @@
+#include <iostream>     // For standard I/O operations (cin, cout)
+#include <cstring>      // For using C-style stringss      
+#include <map>     
+#include <algorithm>    // For using STL algorithms like sort()
+using namespace std;
+
+int main() {
+    // Creation of map
+        map<int, char> meraMap;
+
+    //Insert elements in map
+        meraMap[1] = 'a';
+        meraMap[2] = 'b';
+        meraMap[3] = 'c';
+
+    //accessing elements in map
+        cout << "Your ans is: " << meraMap[2] << endl; // Outputs: b
+}
