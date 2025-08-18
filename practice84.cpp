bf866dd60a839ecd31e4a3949a4934f585720857
Moinkhan21 <moinmykhan07@gmail.com>
2025-08-18 12:16:48 +0530

    code updated

--- practice84.cpp
+++ practice84.cpp
@@ -23,6 +23,27 @@ int main() {
     cout << "Address of c is: " << &c << endl;
     cout << "value stored at ptr is: " << ptr << endl;
     cout << "Value of c is pointing to: " << *ptr << endl;
+    cout << "Address of ptr is: " << &ptr << endl; // Print the address of pointer 'ptr'
+
+    int d = 5;
+    int* ptr2 = &d; // Create another pointer pointing to 'd'
+
+    cout << sizeof(ptr2) << endl; // Print the size of pointer 'ptr2'
+
+    char ch = 'b';
+    char* ptr3 = &ch; // Create a pointer pointing to 'ch'
+
+    cout << sizeof(ptr3) << endl; // Print the size of pointer 'ptr3'
+
+    double e = 5.5;
+    double* ptr4 = &e; // Create a pointer pointing to 'e'
+
+    cout << sizeof(ptr4) << endl; // Print the size of pointer 'ptr4'
+
+    // Bad Practice
+    int* ptr5;
+    // ptr5 is declared but not initialized, it points to an undefined location
+    cout << *ptr5 << endl; // This will lead to undefined behavior
 
     return 0;
 }
