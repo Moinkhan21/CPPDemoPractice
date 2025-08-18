afc60019123d743170a1fe3d730a0d2238043cf6
Moinkhan21 <moinmykhan07@gmail.com>
2025-08-18 17:05:44 +0530

    more code added to pointer

--- practice84.cpp
+++ practice84.cpp
@@ -52,5 +52,37 @@ int main() {
     int* ptr7 = nullptr; // Initialize ptr7 to a null pointer
     cout << ptr7 << endl; // This will also print 0 or NULL
 
+    int s = 5;
+
+    int* ptr8 = &s; // Create a pointer pointing to 's'
+    cout << sizeof(ptr8) << endl; // Print the size of pointer 'ptr8'
+
+    //copy pointer
+    int* ptr9 = ptr8; // Create another pointer pointing to the same address as ptr8
+    cout << sizeof(ptr9) << endl; // Print the size of pointer 'ptr9'
+
+    cout << *ptr8 << endl; // Print the value pointed to by ptr8
+    cout << *ptr9 << endl; // Print the value pointed to by ptr9
+
+    int t = 10;
+    int* ptr10 = &t; // Create a pointer pointing to 't'
+    int* ptr11 = ptr10; // Create another pointer pointing to the same address as ptr10
+    int* ptr12 = ptr11; // Create yet another pointer pointing to the same address as ptr11
+
+    cout << t << endl;
+    cout << &t << endl; // Print the address of variable 't'
+    cout << ptr10 << endl; // Print the address stored in ptr10
+    cout << &ptr10 << endl; // Print the address of pointer 'ptr10'
+    cout << *ptr10 << endl; // Print the value pointed to by ptr10
+    cout << ptr11 << endl; // Print the address stored in ptr11
+    cout << &ptr11 << endl; // Print the address of pointer 'ptr11'
+    cout << *ptr11 << endl; // Print the value pointed to by ptr11
+    cout << ptr12 << endl; // Print the address stored in ptr12
+    cout << &ptr12 << endl; // Print the address of pointer 'ptr12'
+    cout << *ptr12 << endl; // Print the value pointed to by ptr12
+    cout << (*ptr10 + *ptr11 + *ptr12) << endl; // Print the sum of values pointed to by ptr10, ptr11, and ptr12
+    cout << (*ptr10)*2 + (*ptr12)*3 << endl;
+    cout << (*ptr10/2) - (*ptr11/2) << endl;
+
     return 0;
 }
