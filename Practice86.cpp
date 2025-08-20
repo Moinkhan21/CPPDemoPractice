faede6bee65720f44f7eed7a41fb7b0b8d757ca9
Moinkhan21 <moinmykhan07@gmail.com>
2025-08-20 11:16:26 +0530

    pointers' code updated

--- Practice86.cpp
+++ Practice86.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-
 using namespace std;
+
+// void solve(int arr[]) {
+//     cout << "Size inside solve function " << sizeof(arr) << endl;
+// }
 
 int main() {
     char ch[10] = "Babbar";
@@ -26,6 +29,17 @@ int main() {
     cout << c1 + 2 << endl; // Print the address of the third character in name
     cout << *c1 << endl; // Print the first character of name
     cout << c1 + 8 << endl; // Print the address of the last character in name
+
+    char character = 'k';
+    char* cptr = &character;
+
+    cout << cptr << endl;
+
+    char name1[10] = "babbar";
+    char* ch1 = "babbar";
+
+    cout << name1 << endl;
+    cout << ch1 << endl;
 
     return 0;
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+void solve(int arr[]) {
+//     cout << "Size inside solve function " << sizeof(arr) << endl;
+// }
