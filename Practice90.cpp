868656f74acf7283ad6800217feb83bef1104e7e
Moinkhan21 <moinmykhan07@gmail.com>
2025-08-22 10:46:34 +0530

    slow exponentiation

---
+++ Practice90.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int slowExponentiation(int a, int b){
+    int ans = 1;
+    for(int i = 0; i < b; ++i){
+        ans*=a;
+    }
+    return ans;
+} // 0(b)
+
+int main() {
+    cout << slowExponentiation(5, 4) << endl;
+    return 0;
+}
