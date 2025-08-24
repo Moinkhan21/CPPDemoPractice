925af6f4b657dddef30b81450ad230a4502e1f7f
Moinkhan21 <moinmykhan07@gmail.com>
2025-08-24 11:04:23 +0530

    reference variable

---
+++ Practice94.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    //Creating a reference variable
+    int& b = a;
+
+    cout << a << endl;
+    cout << b << endl;
+
+    a++;
+    cout << a << endl;
+    cout << b << endl;
+
+    b++;
+    cout << a << endl;
+    cout << b << endl;
+
+    return 0;
+}
