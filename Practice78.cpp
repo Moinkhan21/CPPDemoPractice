c5bbd6655c2d160ae666ca613dd1300aff72e955
Moinkhan21 <moinmykhan07@gmail.com>
2025-08-31 10:54:19 +0530

    find index of first occurence

--- Practice78.cpp
+++ Practice78.cpp
@@ -2,28 +2,41 @@
 #include <string>
 using namespace std;
 
-int strStr(string haystack, string needle){
-    int n = haystack.size();
-    int m = needle.size();
-    for(int i = 0; i <= n - m; i++) {
+// Function to find the first occurrence of 'needle' in 'haystack'
+int strStr(string haystack, string needle) {
+    int n = haystack.size(); // Length of the main string (haystack)
+    int m = needle.size();   // Length of the substring (needle)
+
+    // Loop through haystack, but only until (n - m), because
+    // if there are fewer than 'm' characters left, needle cannot fit
+    for (int i = 0; i <= n - m; i++) {
         
-        for(int j = 0; j < m; j++) {
-            if(needle[j] != haystack[i + j]) {
+        // For each position 'i', check if the substring starting at 'i' matches needle
+        for (int j = 0; j < m; j++) {
+            
+            // Compare each character of needle with corresponding character in haystack
+            if (needle[j] != haystack[i + j]) {
+                // If any character does not match, break and move to next position of haystack
                 break;
             }
-            if(j == m - 1) {
-                return i; // Found the needle
+
+            // If we reached the last character of needle (j == m - 1) 
+            // and all previous characters matched, we found the needle
+            if (j == m - 1) {
+                return i; // Return the starting index where needle is found
             }
         }
-        
     }
-    return -1; // Needle not found
+
+    // If no match is found after scanning entire haystack, return -1
+    return -1;
 }
 
 int main() {
-    string haystack = "hello";
-    string needle = "ll";
-    int result = strStr(haystack, needle);
-    cout << "Result: " << result << endl;
+    string haystack = "hello";  // Main string
+    string needle = "ll";       // Substring to search
+    int result = strStr(haystack, needle); // Call the function
+
+    cout << "Result: " << result << endl; // Output the result index
     return 0;
 }
