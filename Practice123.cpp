81adb236a3bc267374a7bf1308137f24cfc141cf
Moinkhan21 <moinmykhan07@gmail.com>
2025-09-13 10:29:45 +0530

    printing all subarrays using recursion

---
+++ Practice123.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printSubarray_util(vector<int> &nums, int start, int end) {
+    //Base case
+    if(end == nums.size()){
+        return;
+    }
+
+
+
+    //Solve only one case
+    for(int i = start; i <= end; i++) {
+        cout << nums[i] << " ";
+
+    }
+    cout << endl;
+
+    //Recursive call
+    printSubarray_util(nums, start, end + 1);
+    
+}
+
+void printSubarray(vector<int> &nums){
+    for(int start = 0; start < nums.size(); start++) {
+        int end = start;
+        printSubarray_util(nums, start, end);
+    }
+}
+
+int main() {
+    vector<int> nums{1, 2, 3, 4, 5};
+
+    printSubarray(nums);
+    return 0;
+}
