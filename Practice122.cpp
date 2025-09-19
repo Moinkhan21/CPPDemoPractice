42fa6fac75efbb4496c116dbddc47f2ab2325e59
Moinkhan21 <moinmykhan07@gmail.com>
2025-09-19 16:51:06 +0530

    Substring remove using recursion

--- Practice122.cpp
+++ Practice122.cpp
@@ -3,36 +3,60 @@
 
 using namespace std;
 
-void removeOccure(string& s, string& part){
+// Recursive helper function to remove all occurrences of `part` from `s`
+void removeOccure(string& s, string& part) {
+    // Find the first occurrence of substring `part` in string `s`
     int found = s.find(part);
-    if(found != string::npos) {
-        //Part string has been located
-        //Please remove it
+
+    // If `part` is found inside `s`
+    if (found != string::npos) {
+        // ---------------- SPLITTING THE STRING ----------------
+        // left_part -> substring of `s` before the found index
         string left_part = s.substr(0, found);
+
+        // right_part -> substring of `s` after removing `part`
         string right_part = s.substr(found + part.size(), s.size());
+
+        // Update `s` by concatenating left and right parts (removing `part`)
         s = left_part + right_part;
+
+        // ---------------- RECURSION STEP ----------------
+        // Continue searching and removing more occurrences of `part`
         removeOccure(s, part);
     }
-    else{
-        //base case
-        //all the occurrences of part has been removed from s.
+    else {
+        // ---------------- BASE CASE ----------------
+        // If no more occurrences of `part` are found in `s`
+        // Stop recursion (all instances removed)
         return;
     }
 }
 
+// Main function to remove all occurrences of `part` from `s`
 string removeOccurrences(string s, string part) {
+    // Call helper function to recursively remove substrings
     removeOccure(s, part);
 
+    // Return the modified string after removal
     return s;
 }
 
-int main(){
+int main() {
     string s, part;
+
+    // Input the main string
     cout << "Enter string: ";
     cin >> s;
+
+    // Input the substring to be removed
     cout << "Enter part string to be removed: ";
     cin >> part;
+
+    // Call function and store result
     string ans = removeOccurrences(s, part);
-    cout << ans << endl;
+
+    // Print final string after all occurrences are removed
+    cout << "Final string after removal: " << ans << endl;
+
     return 0;
 }
