a03222103cc06500f5b24e9434b57c5b3dd339ef
Moinkhan21 <moinmykhan07@gmail.com>
2025-10-06 16:17:17 +0530

    Public, Private and Protected

--- Practice150.cpp
+++ Practice150.cpp
@@ -3,9 +3,31 @@ using namespace std;
 
 class Animal {
 
+    public:
+    //State & Properties
+    int age;
+    string type;
+
+    //Behaviour & Functions
+    void eat() {
+        cout << "Eating..." << endl;
+    }
+    void sleep() {
+        cout << "Sleeping..." << endl;
+    }
+
 };
 
 int main() {
     cout << "Size of Empty class is: " << sizeof(Animal) << endl;
+
+    Animal ramesh;
+    ramesh.age = 10; // Error: 'int Animal::age' is private within this context
+    ramesh.type = "Lion";
+    cout << "Age of ramesh is: " << ramesh.age << endl;
+    cout << "Name of ramesh is: " << ramesh.type << endl;
+
+    ramesh.eat(); // Error: 'void Animal::eat()' is private within this context
+    ramesh.sleep();
     return 0;
 }
