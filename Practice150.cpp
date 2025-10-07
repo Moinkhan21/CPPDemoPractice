73e339db22711e285a4d4f107dd451024002cf2a
Moinkhan21 <moinmykhan07@gmail.com>
2025-10-07 16:59:21 +0530

    creation and deletion of obj

--- Practice150.cpp
+++ Practice150.cpp
@@ -124,7 +124,19 @@ int main() {
     // b->print();
 
     //Animal* b = new Animal();
-    b->age = 30;
-    
+    // b->age = 30;
+
+    // cout << "a obj creation" << endl;
+    // Animal a;
+    // a.age = 5;
+
+    // cout << "b obj creation" << endl;
+    // Animal* b = new Animal();
+    // b -> age = 15;
+
+    // Manually
+    // delete b;
+
+
     return 0;
 }
