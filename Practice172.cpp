c89a3595d1d41f3ec63ed9b096be5832ca5ead01
Moinkhan21 <moinmykhan07@gmail.com>
2025-10-14 11:27:48 +0530

    inline function

--- Practice172.cpp
+++ Practice172.cpp
@@ -4,7 +4,13 @@ using namespace std;
 void numberShow(int num) {
     cout << num << endl;
 }
+
+inline void numberShowMultipleOfTwo(int num) {
+    cout << num*2 << endl;
+}
+
 int main(){
     numberShow(10);
+    numberShowMultipleOfTwo(10);
     return 0;
 }
