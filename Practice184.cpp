50da947f677a7dde2bfcc88e1a5f4897b70df124
Moinkhan21 <moinmykhan07@gmail.com>
2025-10-15 16:25:29 +0530

    const keyword in class

---
+++ Practice184.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int x = 5; // X is constant.
+    //Initialization can be done.
+    //but we cant reassing a value.
+
+    cout << x << endl;
+    return 0;
+}
