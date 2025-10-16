5849665d07cbec56b376c76028b0f741bb20ae27
Moinkhan21 <moinmykhan07@gmail.com>
2025-10-16 15:10:45 +0530

    Shallow and Deep on working

---
+++ Practice194.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class abc
+{
+    public:
+        int x;
+        int *y;
+
+        abc(int _x, int _y) : x(_x), y(new int(_y)) {}
+
+        void print() const
+        {
+            printf("X:%d\nPTR Y:%p\nContent of Y (*y):%d\n\n", x, y, *y);
+        }
+};
+
+int main()
+{
+    abc a(1, 2);
+    a.print();
+
+    //abc b(a);
+    abc b = a;
+    b.print();
+    *b.y = 20;
+    b.print();
+
+    a.print();
+    return 0;
+}
