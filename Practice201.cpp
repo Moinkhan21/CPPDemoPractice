c261bf97cea3bc9aa53902707b19ea8c573be857
Moinkhan21 <moinmykhan07@gmail.com>
2025-10-18 11:37:13 +0530

    head & tail for creating linked list

---
+++ Practice201.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+using namespace std;
+
+// Node structure definition
+class Node {
+public:
+    int data;
+    Node* next;
+
+    // Default constructor
+    Node() {
+        this->data = 0;
+        this->next = NULL;
+    }
+
+    // Parameterized constructor
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+// Function to insert a node at the head
+// Pass head by reference (&) so that the original head gets updated
+// I want to insert a node right at the head of Linked List
+void insertAtHead(Node*& head, Node* &tail, int data) {
+
+    if(tail == NULL) {
+        Node* newNode = new Node(data);
+        tail = newNode;
+        head = newNode;
+        return;
+    }
+  
+    // Step 1: Create a new node
+    Node* newNode = new Node(data);
+
+    // Step 2: Point the new node's next to current head
+    newNode->next = head;
+
+    // Step 3: Move head to point to new node
+    head = newNode;
+}
+
+//I want to insert a node right at the end of Linked List
+void insertAtTail(Node* head, Node* &tail, int data) {
+
+    if(tail == NULL) {
+        Node* newNode = new Node(data);
+        tail = newNode;
+        head = newNode;
+        return;
+    }
+
+    //Step 1: create a node
+    Node* newNode = new Node(data);
+
+    //Step 2: connect with tail node 
+    tail -> next = newNode;
+
+    //step 3: update tail;
+    tail = newNode;
+}
+
+// Function to print the linked list
+void print(Node* head) {
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    Node* head = NULL;  // Empty list
+
+    Node* tail = NULL; 
+    // Insert nodes at the head
+    insertAtHead(head, tail, 20);
+    insertAtHead(head, tail, 50);
+    insertAtHead(head, tail, 60);
+    insertAtHead(head, tail, 90);
+    insertAtTail(head, tail, 77);
+
+    cout << "Printing the Linked List:" << endl;
+    print(head);
+
+    return 0;
+}
