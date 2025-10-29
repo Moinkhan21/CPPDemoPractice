e7c5f3a489e36539b7fb391802c3ebda516eb2de
Moinkhan21 <moinmykhan07@gmail.com>
2025-10-29 09:13:18 +0530

    LL is created

---
+++ practice219.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int data){
+        this -> data = data;
+        this -> next = NULL;
+    }
+};
+
+void print(Node* head) {
+    Node* temp = head;
+    while(temp != NULL) {
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+    cout << endl;
+}
+
+int main() {
+    Node* head = new Node(1);
+    Node* second = new Node(2);
+    Node* third = new Node(2);
+    Node* fourth = new Node(0);
+    Node* fifth = new Node(0);
+    Node* sixth = new Node(0);
+
+    head -> next = second;
+    second -> next = third;
+    third -> next = fourth;
+    fourth -> next = fifth;
+    fifth -> next = sixth;
+
+    cout << "Input LL : ";
+    print(head);
+
+    return 0;
+}
