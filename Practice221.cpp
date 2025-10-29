5e4a4a5a17df57c58a239eaaf03c2f85acde90a3
Moinkhan21 <moinmykhan07@gmail.com>
2025-10-29 15:59:36 +0530

    Full code of Addition in linked list

--- Practice221.cpp
+++ Practice221.cpp
@@ -46,6 +46,12 @@ Node* solve(Node* &head1, Node* &head2) {
 
     while(head1 != NULL && head2 != NULL) {
 
+        if(head1 == NULL) 
+            return head2;
+
+        if(head2 == NULL)
+            return head1;
+
         //Calculate sum
         int sum = carry + head1 -> data + head2 -> data;
 
