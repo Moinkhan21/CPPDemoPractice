3cdc6646e1c87b8c0c084c9c2d9b02b069f34b64
Moinkhan21 <moinmykhan07@gmail.com>
2025-11-03 11:40:19 +0530

    maxi and mini nodes between critical points

--- Practice231.cpp
+++ Practice231.cpp
@@ -1,44 +1,70 @@
 #include <iostream>
+#include <vector>
+#include <climits>
 using namespace std;
 
+// Singly Linked List Node Structure
+class ListNode {
+public:
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+// Function to calculate min and max distance between critical points
 vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-    vector<int> ans = {-1, -1}; // minDist, maxDist
-    ListNode* prev = head;
-    if(!prev) return ans;
-    ListNode* curr = head -> next;
-    if(!curr) return ans;
-    ListNode* nxt = head -> next -> next;
-    if(!nxt) return ans;
-
-    int firstCP = -1;
-    int lastCP = -1;
-    int minDist = INT_MAX;
-    int i = 1;
-    while(nxt) {
-        bool isCP = ((curr -> val > prev -> val && curr -> val > nxt -> val) || 
-                (curr -> val < prev -> val && curr -> val < nxt -> val)) ? true : false;
-        
-        if(isCp && firstCp == -1) {
-            firstCP = i;
-            lastCP = i;
-
-        }
-        else if(isCP) {
-            minDist = min(minDist, i - lastCP);
-            lastCP = i;
-        }
-        ++i;
-        prev = prev -> next;
-        curr = curr -> next;
-        nxt = nxt -> next;
-    }
-    if(lastCP == firstCP) {
-        //Only 1 CP was found.
+    vector<int> ans = {-1, -1};
+    if (!head || !head->next || !head->next->next)
         return ans;
+
+    ListNode* prev = head;
+    ListNode* curr = head->next;
+    ListNode* nxt = head->next->next;
+
+    int firstCP = -1, lastCP = -1;
+    int minDist = INT_MAX;
+    int index = 1;
+
+    while (nxt) {
+        bool isCP = ((curr->val > prev->val && curr->val > nxt->val) ||
+                     (curr->val < prev->val && curr->val < nxt->val));
+
+        if (isCP && firstCP == -1) {
+            firstCP = index;
+            lastCP = index;
+        } 
+        else if (isCP) {
+            minDist = min(minDist, index - lastCP);
+            lastCP = index;
+        }
+
+        prev = curr;
+        curr = nxt;
+        nxt = nxt->next;
+        index++;
     }
-    else{
-        ans[0] = minDist;
-        ans[1] = lastCP - firstCP;
-    }
+
+    if (firstCP == lastCP)
+        return ans;
+
+    ans[0] = minDist;
+    ans[1] = lastCP - firstCP;
     return ans;
+}
+
+// ✅ MAIN FUNCTION
+int main() {
+    // Example Linked List: 1 -> 3 -> 2 -> 5 -> 1
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(3);
+    head->next->next = new ListNode(2);
+    head->next->next->next = new ListNode(5);
+    head->next->next->next->next = new ListNode(1);
+
+    vector<int> result = nodesBetweenCriticalPoints(head);
+
+    cout << "Min Distance: " << result[0] << endl;
+    cout << "Max Distance: " << result[1] << endl;
+
+    return 0;
 }
