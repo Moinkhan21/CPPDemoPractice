169dbe07feac9aac0000fbc9ffa5c1e937bf1406
moinmykhan07@gmail.com <moinmykhan07@gmail.com>
2026-01-08 11:42:47 +0530

    priority queue as max heap

---
+++ Practice433.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    //Max-heap
+    priority_queue<int> pq;
+    pq.push(3);
+    pq.push(6);
+    pq.push(9);
+    pq.push(4);
+    pq.push(8);
+
+    cout << "Top element: " << pq.top() << endl;
+    pq.pop();
+    cout << "Top element: " << pq.top() << endl;
+    pq.pop();
+    cout << "Top element: " << pq.top() << endl;
+    pq.pop();
+    cout << "Top element: " << pq.top() << endl;
+    pq.pop();
+    cout << "Top element: " << pq.top() << endl;
+    pq.pop();
+    cout << "Size: " << pq.size() << endl;
+
+    return 0;
+}
