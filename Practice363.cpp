9a93479cd47474f596b0147df5c45bb19467d509
moinmykhan07@gmail.com <moinmykhan07@gmail.com>
2026-01-26 11:19:48 +0530

    Printing all entries of map

---
+++ Practice363.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main() {
+    //Creation
+    unordered_map<string, int> m;
+
+    //Insertion
+    pair<string, int> p = make_pair("Scorpio", 9);
+    m.insert(p);
+
+    pair<string, int> p1("Alto", 2);
+    m.insert(p1);
+
+    m["Fortuner"] = 10;
+
+    //Access
+    cout << m.at("Alto") << endl;
+    cout << m.at("Scorpio") << endl;
+    cout << m["Fortuner"] << endl;
+
+    //Search OR Find
+    cout << m.count("Alto") << endl;
+    cout << m.count("Innova") << endl;
+    if(m.find("Fortuner") != m.end()) {
+        cout << "Fortuner is found" << endl;
+    }
+    else {
+        cout << "Fortuner is not found" << endl;
+    }
+
+    cout << m.size() << endl;
+
+    //Exception case
+    cout << m["Hummer"] << endl;
+    cout << m.size() << endl;
+
+    cout << "Printing all entries: " << endl;
+    for(auto i : m) {
+        cout << i.first << " -> " << i.second << endl; 
+    }
+
+    return 0;
+}
