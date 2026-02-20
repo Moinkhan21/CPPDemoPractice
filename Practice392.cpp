b117983d332c758de3d2afc6872c29b9310200b6
moinmykhan07@gmail.com <moinmykhan07@gmail.com>
2026-02-20 05:30:46 +0530

    Findig given value is even or odd

---
+++ Practice392.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Enter integer the value: " << endl;
+    int n;
+    cin >> n;
+
+    if(n & 1) {
+        cout << n << " is odd" << endl;
+    }
+    else {
+        cout << n << " is even" << endl;
+    }
+
+    return 0;
+}
