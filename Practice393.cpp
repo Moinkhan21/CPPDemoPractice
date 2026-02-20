9516fc887cd91ac1719e72a3d52f2ee95874aa93
moinmykhan07@gmail.com <moinmykhan07@gmail.com>
2026-02-20 05:45:46 +0530

    Bit masking basic

---
+++ Practice393.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int getIthBit(int n, int i) {
+    int mask = 1 << i;
+    int ans = n & mask;
+    if(ans == 0)
+        return 0;
+    else
+        return 1;
+}
+
+int main() {
+
+    cout << "Enter integer value: " << endl;
+    int n;
+    cin >> n;
+
+    cout << "Enter value of i for bit masking: " << endl;
+    int i;
+    cin >> i;
+
+    int ans = getIthBit(n, i);
+    cout << "After performing operation of bit masking the answer is: " << endl;
+    cout << ans << endl;
+
+    return 0;
+}
