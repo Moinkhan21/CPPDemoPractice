597b3e1cd69a6bfe8a8a36e3b1313c53f75df12d
moinmykhan07@gmail.com <moinmykhan07@gmail.com>
2026-03-25 09:41:38 +0530

    Static keyword in class

--- Practice180.cpp
+++ Practice180.cpp
@@ -1,22 +1,50 @@
 #include <iostream>
 using namespace std;
 
-class abc
-{
-    public:
-        int x, y;
+// ======================================================================
+// CLASS: abc
+// ----------------------------------------------------------------------
+// Demonstrates the use of the `this` pointer in C++.
+//
+// DATA MEMBERS:
+//   • x → integer value
+//   • y → integer value
+//
+// KEY CONCEPT:
+//   `this` pointer refers to the CURRENT OBJECT
+// ======================================================================
+class abc {
+public:
+    int x, y;
 
-        void print() const
-        {
-            //this
-            cout << this->x << " " << this->y << endl;
-        }
+    // --------------------------------------------------
+    // FUNCTION: print()
+    // --------------------------------------------------
+    // PURPOSE:
+    //   Prints values of x and y using `this` pointer.
+    //
+    // WHAT IS `this`?
+    //   • A pointer to the current object
+    //   • Automatically passed to all non-static member functions
+    //
+    void print() const {
+
+        // Accessing members using this pointer
+        cout << this->x << " " << this->y << endl;
+    }
 };
 
+// ======================================================================
+// MAIN FUNCTION
+// ======================================================================
 int main() {
+
+    // Object initialization
     abc obj1 = {1, 2};
     abc obj2 = {4, 5};
-    obj1.print();
-    obj2.print();
+
+    obj1.print();   // Output: 1 2
+    obj2.print();   // Output: 4 5
+
     return 0;
 }
